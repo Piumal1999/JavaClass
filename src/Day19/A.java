0497b18e7f72be4fb0b1b9cbd1cc32b20f0c1b88
piumal1999 <piumalrathnayake@hotmail.com>
2019-09-19 11:12:36 +0530

    Add day 19 class work

--- src/Day19/A.java
+++ src/Day19/A.java
@@ -3,6 +3,7 @@ package Day19;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,11 +17,26 @@ public class A extends Application {
         Button b1 = new Button();
         b1.setText("Click");
 
-        VBox layout = new VBox();
-        layout.getChildren().add(b1);
+        Button b2 = new Button();
+        Button b3 = new Button();
+        b2.setText("Don't Click");
+        b3.setText("Don't Click");
+
+        HBox row1 = new HBox();
+        HBox row2 = new HBox();
+
+        row1.getChildren().add(b1);
+        row1.getChildren().add(b2);
+
+        row2.getChildren().add(b1);
+
+        VBox layout = new VBox(); //vertical Box
+        layout.getChildren().add(row1);
+        layout.getChildren().add(row2);
 
         Scene scene1 = new Scene(layout, 400,300);
         primaryStage.setScene(scene1);
+
 
         primaryStage.show();
     }
